e546993e17515cd3dbffc46ac85be33ae611d41e
Natasha Henry <natashaphenry@yahoo.com>
2019-06-21 01:11:19 -0400

    updated file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import Board from './components/board'
-import initializeDeck from './deck'
+import React, { Component } from "react";
+import AnimeCard from "./components/AnimeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import animes from "./anime.json";
+import Navbar from "./components/Navbar";
 
-export default function App() {
-  const [cards, setCards] = useState([])
-  const [flipped, setFlipped] = useState([])
 
-  useEffect(() => {
-    setCards(initializeDeck())
-  }, [])
 
-  const handleClick = (id) => setFlipped([... flipped, id])
-  
-  return (
-    <div>
-      <p>Click on an image to earn points, but don't click on any more than once!</p>
-    <Board cards={cards} flipped={flipped} handleClick=
-    {handleClick} />
 
-    </div>
-  )      
+class App extends Component {
+  state = {
+    animes, 
+    clickedAnimes: [],
+    score: 0
+  };
+
+
+  imageClick = event => {
+    const currentPicture = event.target.src;
+    const pictureClickedAlready =
+      this.state.clickedAnime.indexOf(currentPicture) > -1;
+
+      if (pictureClickedAlready) {
+        this.setState({
+          animes: this.state.animes.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedAnimes: [],
+          score: 0
+        });
+          alert("You guessed incorrectly!");
+
+
+    } else {
+      this.setState(
+        {
+          animes: this.state.animes.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedAnimes: this.state.clickedAnimes.concat(
+            currentPicture
+          ),
+          score: this.state.score + 1
+        },
+     
+        () => {
+          if (this.state.score === 14) {
+            alert("You guessed correctly!");
+            this.setState({
+              animes: this.state.animes.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedAnimes: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+  render() {
+    return (
+      <Wrapper>
+             <Navbar 
+          score={this.state.score}
+        />
+        <Title><h1>Clicky Game</h1>
+        </Title>
+        {this.state.animes.map(anime => (
+          <AnimeCard
+            imageClick={this.imageClick}
+            id={anime.id}
+            key={anime.id}
+            image={anime.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
+export default App;
 
