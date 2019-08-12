8431802deeed107f2b59787bf1255bb6177d9781
Natasha Henry <natashaphenry@yahoo.com>
2019-08-12 19:48:55 -0400

    Updates

---
+++ precache-manifest.c7458564689544abc84e4623fe62e77c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d65dbfd69f74f39bbb929ea7a3ef1f",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "b18940a6ecfdc35f7de1",
+    "url": "/clicky_game/static/css/main.5886a14c.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky_game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "b18940a6ecfdc35f7de1",
+    "url": "/clicky_game/static/js/main.2ebfb71c.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  }
+]);
